123586980939b2e2d41163c1965f0d400465cc7d
LacoAncaLaura <laco.ancalaura@yahoo.com>
2020-03-26 22:16:38 +0200

    new project and doing some maths overloading methods

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -7,24 +7,28 @@ package org.fasttrackit;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Let`s do some maths" );
+    { System.out.println( "Let`s do some maths" );
         Calculator calculator = new Calculator();
-        calculator.number = 2542;
-        calculator.decimals = 654.26;
-        calculator.anotherNumber = 3153;
-        calculator.anotherDecimals=52525.352;
 
-        int total = calculator.count(2542 , 3153);
-        double total1 = calculator.counting(654.26 , 52525.352);
+        int total = calculator.count(254 , 313);
+        double total1 = calculator.counting(654.26 , 52.352);
         int result = calculator.decrease(2542, 3153);
-        double result1 = calculator.decreasing(52525.352 , 654.26);
+        double result1 = calculator.decreasing(52.352 , 654.26);
         calculator.summing(calculator , 2542);
         int result2 = calculator.multiplying(2542);
-        calculator.divide(calculator,52525.352);
+        calculator.divide(calculator,525.352);
 
+        int count = calculator.count(25 +  153);
+        int decrease = calculator.decrease( 252 -  150);
+        double counting = calculator.counting(654.6 ,15.3);
+        double decreasing = calculator.decreasing(364.3 - 3.2);
+        calculator.summing(2542 , calculator);
+        int multiplying = calculator.multiplying(365 *2);
+        calculator.divide(525.352/3);
 
     }
+
+
 
 
 
---
+++ src/main/java/org/fasttrackit/Battery.java
@@ -0,0 +1,8 @@
+package org.fasttrackit;
+
+public class Battery {
+    Battery battery;
+
+
+    }
+
